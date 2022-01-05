cc2f355c0c2a6066f445b39ee6d0c90812ed39f4
Thanut Panichyotai <luvikung@gmail.com>
2022-01-06 01:25:08 +0700

    add initial game mechanics such as button, gems, type of gems, rules to random, etc.

---
+++ Assets/Contents/Scripts/Exceptions/GameDevelopmentException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToonBlastPuzzle
+{
+    /// <summary>
+    /// Exception for game development with message.
+    /// </summary>
+    public class GameDevelopmentException : Exception
+    {
+        /// <summary>
+        /// (Private this because we don't want to throw exception without message.)
+        /// </summary>
+        private GameDevelopmentException() : base() { }
+        public GameDevelopmentException(string message) : base(message) { }
+        public GameDevelopmentException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Assets/Contents/Scripts/GemLayout.cs
@@ -0,0 +1,12 @@
+﻿using LuviKunG.UI;
+using UnityEngine;
+
+namespace ToonBlastPuzzle
+{
+    public sealed class GemLayout : UIBehaviourBase
+    {
+        [SerializeField]
+        private GemLayoutButton gemLayoutButton;
+        public ref GemLayoutButton button => ref gemLayoutButton;
+    }
+}
