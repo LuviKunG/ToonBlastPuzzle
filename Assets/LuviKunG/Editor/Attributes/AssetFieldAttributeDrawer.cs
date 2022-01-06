9ad316fb2eb49bf2840bc6154e6d6cd500752a1d
Thanut Panichyotai <luvikung@gmail.com>
2022-01-07 01:16:25 +0700

    remove old prefab field and scriptable object field attribute. add new attribute for asset field.

---
+++ Assets/LuviKunG/Editor/Attributes/AssetFieldAttributeDrawer.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+namespace LuviKunG.Attribute
+{
+    using LuviKunG.EditorLayout;
+
+    [CustomPropertyDrawer(typeof(AssetFieldAttribute))]
+    public class AssetFieldAttributeDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType == SerializedPropertyType.ObjectReference)
+            {
+                Rect rectField = EditorGUI.PrefixLabel(position, label);
+                if (property.objectReferenceValue == null || !AssetDatabase.Contains(property.objectReferenceValue))
+                {
+                    using (new EditorColorScope(Color.red))
+                    {
+                        EditorGUI.PropertyField(rectField, property, GUIContent.none);
+                    }
+                }
+                else
+                {
+                    EditorGUI.PropertyField(rectField, property, GUIContent.none);
+                }
+            }
+            else
+            {
+                EditorGUI.HelpBox(position, $"Cannot use {nameof(AssetFieldAttributeDrawer)} to the field that didn't from object type.", MessageType.Error);
+            }
+        }
+    }
+}
---
+++ Assets/LuviKunG/Scripts/Attributes/AssetFieldAttribute.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace LuviKunG.Attribute
+{
+    /// <summary>
+    /// Makes object field require object reference from asset.
+    /// </summary>
+    public class AssetFieldAttribute : PropertyAttribute
+    {
+        public AssetFieldAttribute() { }
+    }
+}
